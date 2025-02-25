3039125da173a208ef3e77c99294bde191174d20
Vincent Leroy <vincent.leroy@rayference.eu>
2025-02-25 23:49:17 +0100

    Add package version number

--- src/xarray_validate/__init__.py
+++ src/xarray_validate/__init__.py
@@ -3,6 +3,7 @@ Main interface.
 """
 
 from . import types as types
+from ._version import version as __version__
 from .base import SchemaError
 from .components import (
     ArrayTypeSchema,
@@ -18,6 +19,7 @@ from .dataarray import CoordsSchema, DataArraySchema
 from .dataset import DatasetSchema
 
 __all__ = [
+    "__version__",
     "SchemaError",
     "ArrayTypeSchema",
     "AttrSchema",
