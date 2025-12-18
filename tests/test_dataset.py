3e4963ed6c2c68df1554c9b882d7cc3514196225
Vincent Leroy <vincent.leroy@rayference.eu>
2025-12-18 12:42:16 +0100

    Make data_vars optional
    This commit propagates behaviour implemented for CoordsSchema and
    AttrsSchema to DatasetSchema, making it possible to make variables
    optional, and to allow unknown data variables.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -11,9 +11,11 @@ def test_dataset_empty_constructor():
     ds_schema = DatasetSchema()
     assert hasattr(ds_schema, "validate")
     assert ds_schema.serialize() == {
+        "require_all_keys": True,
+        "allow_extra_keys": True,
         "attrs": {},
         "data_vars": {},
-    }  # TODO: Check for correctness
+    }
 
 
 def test_dataset_example(ds):
@@ -32,7 +34,7 @@ def test_dataset_example(ds):
         ds_schema.validate(ds)
 
     ds = ds.drop_vars("foo")
-    with pytest.raises(SchemaError, match="variable foo"):
+    with pytest.raises(SchemaError, match="data_vars has missing keys"):
         ds_schema.validate(ds)
 
 
@@ -102,7 +104,7 @@ def test_attrs_missing_key():
     ds_schema = DatasetSchema(
         attrs=AttrsSchema(attrs={name: AttrSchema(value=value)}, allow_extra_keys=False)
     )
-    with pytest.raises(SchemaError):
+    with pytest.raises(SchemaError, match="attrs has extra keys"):
         ds_schema.validate(ds)
 
 
@@ -111,6 +113,8 @@ def test_schema_from_dataset(ds):
     schema.validate(ds)
 
     expected = {
+        "require_all_keys": True,
+        "allow_extra_keys": True,
         "data_vars": {
             "foo": {
                 "dtype": "<i4",
@@ -152,3 +156,75 @@ def test_schema_from_dataset(ds):
         },
     }
     assert schema.serialize() == expected
+
+
+def test_optional_data_vars(ds):
+    """Test that data variables can be made optional with require_all_keys=False."""
+    ds_schema = DatasetSchema(
+        data_vars={
+            "foo": DataArraySchema(name="foo", dtype=np.int32, dims=["x"]),
+            "bar": DataArraySchema(name="bar", dtype=np.floating, dims=["x", "y"]),
+            "optional_var": DataArraySchema(dtype=np.float32),
+        },
+        require_all_keys=False,
+    )
+
+    # Original dataset should validate
+    ds_schema.validate(ds)
+
+    # Dataset missing "optional_var" should still validate
+    ds_schema.validate(ds)
+
+    # Dataset with only "foo" should validate
+    ds_partial = ds.drop_vars("bar")
+    ds_schema.validate(ds_partial)
+
+    # Dataset with only "bar" should validate
+    ds_partial = ds.drop_vars("foo")
+    ds_schema.validate(ds_partial)
+
+    # Empty dataset (no data vars from schema) should validate
+    ds_empty = ds.drop_vars(["foo", "bar"])
+    ds_schema.validate(ds_empty)
+
+
+def test_data_vars_missing_keys_error():
+    """Test that missing required data variables raise an error."""
+    ds = xr.Dataset(
+        {
+            "foo": xr.DataArray([1, 2, 3], dims=["x"]),
+        }
+    )
+
+    # Default behavior: require_all_keys=True
+    ds_schema = DatasetSchema(
+        data_vars={
+            "foo": DataArraySchema(dtype=np.int64),
+            "bar": DataArraySchema(dtype=np.float64),
+        }
+    )
+
+    with pytest.raises(SchemaError, match="data_vars has missing keys"):
+        ds_schema.validate(ds)
+
+
+def test_data_vars_extra_keys_error():
+    """Test that extra data variables raise an error when allow_extra_keys=False."""
+    ds = xr.Dataset(
+        {
+            "foo": xr.DataArray([1, 2, 3], dims=["x"]),
+            "bar": xr.DataArray([1.0, 2.0, 3.0], dims=["x"]),
+            "extra": xr.DataArray([4, 5, 6], dims=["x"]),
+        }
+    )
+
+    ds_schema = DatasetSchema(
+        data_vars={
+            "foo": DataArraySchema(dtype=np.int64),
+            "bar": DataArraySchema(dtype=np.float64),
+        },
+        allow_extra_keys=False,
+    )
+
+    with pytest.raises(SchemaError, match="data_vars has extra keys"):
+        ds_schema.validate(ds)
